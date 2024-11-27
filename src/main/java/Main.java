ca2dc78244342168b0ce629f77998304f59c2dd4
Christian777-Ing <cezavala@espol.edu.ec>
2024-11-26 20:05:15 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,8 @@ import vista.MedicinaVista;
 import vista.MedicoVista;
 import vista.PerfilVista;
 import vista.TomaMedicinaVista;
+import modelo.Perfil;
+import modelo.medicina.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -45,6 +47,24 @@ public class Main {
         medicoModelo, citaMedicaModelo, 
         actividadFisicaModelo, medicinaControlador, medicoControlador, 
         citaMedicaControlador, actividadFisicaControlador);
+
+        Perfil perfilInicio= new Perfil("Christian", "Paciente", "er.kj45@hotmail.com");
+        perfilModelo.agregarPerfil(perfilInicio);
+        medicinaModelo.inicializarListaMedicinaParaPerfil();
+            tomaMedicinaModelo.inicializarListaTomas();
+            medicoModelo.inicializarListaMedicosPerfil();
+            citaMedicaModelo.inicializarListaCitasPerfil();
+            actividadFisicaModelo.inicializarListaActividades();
+        Medicina medicinaInicio= new Medicina("Paracetamol", 30, "PASTILLA","CADA DIA", "UNA VEZ AL DÍA [ 8:00 ]", 500);
+        medicinaModelo.agregarMedicina(0, medicinaInicio);
+        Fecha fechaInicio= new Fecha("Lunes", "8:00");
+        ActividadFisica actividadInicial= new ActividadFisica(fechaInicio, "Trotar", "0:30", "MAÑANA");
+        actividadFisicaModelo.agregarActividad(0, actividadInicial);
+        Medico medicoInicial= new Medico("Jacob", "CARDIOLOGIA", "0968124200", "Jacob@hotmail.com", "Sucre 1324");
+        medicoModelo.agregarMedico(0, medicoInicial);
+        Fecha fechaInicio1= new Fecha("Viernes", "15:00");
+        CitaMedica citaMedicaInicial= new CitaMedica("Cardiologia", fechaInicio1, medicoInicial);
+        citaMedicaModelo.agregarCitaPerfil(0, citaMedicaInicial);
         perfilControlador.inicio();
     }
 }
