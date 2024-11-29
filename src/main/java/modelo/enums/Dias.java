979346f8cae19379ba1c0c2f9221f43cc8f40766
XavierG <ixavierguzmang05@gmail.com>
2024-11-28 22:13:27 -0500

    99.9%

---
+++ src/main/java/modelo/enums/Dias.java
@@ -0,0 +1,11 @@
+package modelo.enums;
+
+public enum Dias {
+    Lunes,
+    Martes,
+    Miercoles,
+    Jueves,
+    Viernes,
+    Sabado,
+    Domingo
+}
---
+++ src/main/java/modelo/enums/Presentacion.java
@@ -0,0 +1,10 @@
+package modelo.enums;
+
+public enum Presentacion {
+    PASTILLA,
+    INYECCION,
+    SOLUCION,
+    GOTAS,
+    INHALADOR,
+    POLVO
+}
